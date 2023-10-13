9ac7fa4d5424c23819cd6ee8957cfbb1487bd637
Jannik Krusch <jannik.krusch@mmmake.com>
2023-10-13 15:12:47 +0200

    feat: added new routes, added attributes to models,

--- src/utils/constants/RouterPaths.ts
+++ src/utils/constants/RouterPaths.ts
@@ -1,6 +1,9 @@
 export const RouterPaths = {
   Default: { path: "/", display: "Default" },
   Error: { path: "/error", display: "Error" },
+  Blogs: { path: "/blogs", display: "Blogs" },
+  Films: { path: "/films", display: "Films" },
+  Series: { path: "/series", display: "Series" },
+  Impress: { path: "/impress", display: "Impress" },
   About: { path: "/about", display: "About" },
-  Users: { path: "/users", display: "Users" },
 } as const;
--- src/utils/models/MotionPicture.ts
+++ src/utils/models/MotionPicture.ts
@@ -3,6 +3,7 @@ export class MotionPicture {
   public director: string = "";
   public actors: string[] = [];
   public rating: number = 0;
+  public ageRating: number = 0;
   public image: string = "";
   public genre: string[] = [];
   public initialRelease: number = 0;
