ba4501e8e7ea15e6d3219b98f411c1cbd9d0ae50
Jannik Krusch <jannik.krusch@mmmake.com>
2023-10-27 14:20:55 +0200

    feat: working on autocomplete navbar

--- src/components/shared/navbar/CustomNavbar.tsx
+++ src/components/shared/navbar/CustomNavbar.tsx
@@ -1,3 +1,4 @@
+import { BlogPost } from "../../../utils";
 import { COMPANY_NAME } from "../../../utils/constants/Names";
 import { RouterPaths } from "../../../utils/constants/RouterPaths";
 import { DataContext } from "../../../utils/context/DataContext";
@@ -7,12 +8,35 @@ import { useContext, useState } from "react";
 import { Navbar, Container, Nav } from "react-bootstrap";
 
 export function CustomNavbar() {
+  const { blogPosts } = useContext(DataContext);
   const [menuActive, setMenuActive] = useState(false);
   const { setSearchQuery } = useContext(DataContext);
+  const [suggestions, setSuggestions] = useState<BlogPost[]>([]);
+  const allPossibleSuggestions = blogPosts;
 
-  const toggleMenu = () => {
-    setMenuActive(!menuActive);
-  };
+  function updateSuggestions(query: string) {
+    if (!query.trim()) {
+      setSuggestions([]);
+      return;
+    }
+
+    const filteredSuggestions = allPossibleSuggestions.filter((suggestion) =>
+      suggestion.relatedMotionPicture.title
+        .toLowerCase()
+        .includes(query.toLowerCase())
+    );
+    setSuggestions(filteredSuggestions);
+  }
+
+  function selectSuggestion(suggestion: BlogPost) {
+    setSearchQuery(suggestion.relatedMotionPicture.title);
+    setSuggestions([]); // Vorschläge leeren, sobald ein Vorschlag ausgewählt wurde
+  }
+
+  function handleInput(query: string): void {
+    setSearchQuery(query);
+    updateSuggestions(query);
+  }
 
   return (
     <Navbar expand="sm" className="custom-navbar" data-bs-theme="dark">
@@ -42,8 +66,19 @@ export function CustomNavbar() {
             <input
               className="w-100 search-box"
               placeholder="search"
-              onChange={(e) => setSearchQuery(e.target.value)}
+              onChange={(e) => handleInput(e.target.value)}
             />
+            <div className="suggestions-container">
+              {suggestions.map((suggestion) => (
+                <div
+                  key={suggestion.id}
+                  onClick={() => selectSuggestion(suggestion)}
+                  className="suggestion-item"
+                >
+                  {suggestion.relatedMotionPicture.title}
+                </div>
+              ))}
+            </div>
           </div>
         </Navbar.Collapse>
       </Container>
